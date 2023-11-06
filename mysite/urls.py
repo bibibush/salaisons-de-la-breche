52c77ab40de10b0b524799773e28ecfccc648d64
Bibibush <rlawlsgus125@gmail.com>
2023-11-06 13:31:08 +0100

    debug=true 변경

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,9 +22,11 @@ from django.views.generic import TemplateView
 from django.views.static import serve
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root' : settings.STATIC_ROOT}),
-    re_path(r'.*', TemplateView.as_view(template_name = 'index.html'), name= 'react-web')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'.*', TemplateView.as_view(template_name='index.html'), name='react'),
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root' : settings.STATIC_ROOT}),
+    # re_path(r'.*', TemplateView.as_view(template_name = 'index.html'), name= 'react-web')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
